610ae85f7e6eba651576e9bd20b480fa0efbef19
Fernando <fer.vandresen0051@gmail.com>
2024-08-10 00:13:35 -0300

    commit incial

---
+++ src/funcoes.js
@@ -0,0 +1,5 @@
+"use strict";
+const multiplicar = (a, b) => a * b;
+console.log(multiplicar(7, 9));
+const falarNome = (nome) => `Olá ${nome}`;
+console.log(falarNome('Fernando'));
---
+++ src/funcoes.ts
@@ -0,0 +1,7 @@
+const multiplicar = (a:number, b:number):number => a * b;
+
+console.log(multiplicar(7, 9))
+
+const falarNome = (nome:string):string => `Olá ${nome}`;
+
+console.log(falarNome('Fernando'))
